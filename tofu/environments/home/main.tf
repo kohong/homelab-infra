99e087db41de9ae37cf1e751b278c08a441d4342
Ko Hong <kohong.ang@gmail.com>
2026-09-06 17:52:22 +0800

    Clean up ansible structure

--- tofu/environments/home/main.tf
+++ tofu/environments/home/main.tf
@@ -12,3 +12,18 @@ module "test" {
   cores  = 2
   memory = 2048
 }
+
+module "mgmt_01" {
+  source = "../../modules/debian-vm"
+
+  vm_id = 190
+  name  = "mgmt-01"
+
+  node_name      = var.proxmox_node
+  template_vm_id = var.template_vm_id
+  datastore      = var.vm_datastore
+  bridge         = var.vm_bridge
+
+  cores  = 2
+  memory = 4096
+}
